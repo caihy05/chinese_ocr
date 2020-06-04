3e8312e9d34af4df596f609a29e0d4c561426984
caihy05 <228417442@qq.com>
2020-06-04 10:44:48 +0800

    1、添加conda创建虚拟环境 2、固定keras版本

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+# conda create -n chineseocr python=3.6 pip
+# conda activate chineseocr
 pip install numpy scipy matplotlib pillow
 pip install easydict opencv-python keras h5py PyYAML
 pip install cython==0.24
+pip install keras==2.1.2
 
 # for gpu
 pip install tensorflow-gpu==1.3.0
